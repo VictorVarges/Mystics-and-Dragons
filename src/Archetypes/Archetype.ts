ec5f82b16052a476e85c89baa8dcb07a20dee587
Victor Varges <victorryandias@gmail.com>
2022-03-25 21:35:08 -0300

    requirement 4: class creation with name, special and cost attributes, static getter returned message and abstract getter returns a type with possible response being mana or stamina. In addition to the getter returned the value of each attribute

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,33 @@
+import { EnergyType } from '../Energy';
+
+abstract class Archetype {
+  private _name: string;
+  private _special: number;
+  private _cost: number;
+
+  constructor(name: string) {
+    this._name = name;
+    this._special = 0;
+    this._cost = 0;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get special() {
+    return this._special;
+  }
+
+  get cost() {
+    return this._cost;
+  }
+
+  static createdArchetypeInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get energyType(): EnergyType;
+}
+
+export default Archetype;
