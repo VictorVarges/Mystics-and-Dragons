d36604882dbfdef1d2f4379acbc0f2cb335ca354
Victor Varges <victorryandias@gmail.com>
2022-03-28 10:51:16 -0300

    requirement 10: PVP class implementation inherited by the Battle class, which has fight as its method.

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,21 @@
+import Battle from './Battle';
+import Fighter from '../Fighter';
+
+class PVP extends Battle {
+  private _player1: Fighter;
+  private _player2: Fighter;
+
+  constructor(p1: Fighter, p2: Fighter) {
+    super(p1);
+    this._player1 = p1;
+    this._player2 = p2;
+  }
+
+  fight(): number {
+    this._player1.attack(this._player2);
+    this._player2.attack(this._player1);
+    return this._player1.lifePoints <= 0 ? -1 : 1;
+  } 
+}
+
+export default PVP;
--- src/Battle/index.ts
+++ src/Battle/index.ts
@@ -1,3 +1,5 @@
 import Battle from './Battle';
+import PVP from './PVP';
 
+export { PVP };
 export default Battle;
